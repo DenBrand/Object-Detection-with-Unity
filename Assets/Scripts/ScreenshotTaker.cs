af0d60817d5c216439618312ab6bfbaacd9d730f
DenBrand <dennis.brand-r5c@rub.de>
2020-12-03 13:08:25 +0100

    Fixed a cruicial bug which caused generation of falsy data when spamming the capturing key rapidly. Now the game writes a message in the info box if captures was not taken because of that. Added some additional data.

--- Assets/Scripts/ScreenshotTaker.cs
+++ Assets/Scripts/ScreenshotTaker.cs
@@ -17,12 +17,15 @@ public class ScreenshotTaker: MonoBehaviour {
     public int maxMessages;
     [SerializeField] List<Message> messageList = new List<Message>();
     public GameObject contentObject, textObject;
+    public float deathHeight;
+    public GameObject player;
+    private bool allowCapturing;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Set Cursor to not visible
         Cursor.visible = false;
+        allowCapturing = true;
 
         if(SystemInfo.operatingSystem.StartsWith("Windows"))
             path = "screenshots\\";
@@ -59,13 +62,32 @@ public class ScreenshotTaker: MonoBehaviour {
     void Update()
     {
 
+        if(player.transform.position.y <= deathHeight) {
+
+            sendMessage("<color=red>You fell out of bounds. I reset you to the spawn location.</color>");
+            player.transform.position = Vector3.zero;
+
+        }
+
         if(Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
 
         if(Input.GetKeyDown(KeyCode.E)) {
+            if(allowCapturing) {
 
-            // start coroutine so hide UI for the frame the screen is captured
-            StartCoroutine(CaptureObjects());
+                allowCapturing = false;
 
+                // start coroutine so hide UI for the frame the screen is captured
+                StartCoroutine(CaptureObjects());
+
+                StartCoroutine(AllowCapturingAfterOneSecond(1.1f));
+
+            } else {
+
+                sendMessage("<color=red>Nothing captured. Please wait at least one second. That's because the " +
+                            "images are named with a timestamp messured in seconds. Multiple captures " +
+                            "within the same second would lead to overwritten images and faulty labels.</color>");
+
+            }
         }
 
         if(Input.GetKeyDown(KeyCode.H)) {
@@ -74,9 +96,15 @@ public class ScreenshotTaker: MonoBehaviour {
 
         }
 
-        IEnumerator CaptureObjects() {
+        // extra mechanism to prevent overwriting images
+        IEnumerator AllowCapturingAfterOneSecond(float n) {
 
-            sendMessage("*SNAP*");
+            yield return new WaitForSecondsRealtime(n);
+            allowCapturing = true;
+
+        }
+
+        IEnumerator CaptureObjects() {
 
             // get time stamp as file name
             System.DateTime time = System.DateTime.Now;
@@ -87,152 +115,171 @@ public class ScreenshotTaker: MonoBehaviour {
                             + time.Minute + "min"
                             + time.Second + "sec";
 
-            // wait till the last possible moment before screen rendering to hide UI
-            bool canvasWasEnabled = UICanvas.enabled;
-            yield return null;
-            UICanvas.enabled = false;
+            int randomNumber = Random.Range(0, 9999);
 
-            // wait for screen rendering to complete
-            yield return new WaitForEndOfFrame();
-            if(canvasWasEnabled) UICanvas.enabled = true;
+            if(!File.Exists(path + timestmp + randomNumber + ".jpg")) {
 
-            // take screenshot and save raw version
-            Texture2D screenshot = ScreenCapture.CaptureScreenshotAsTexture();
-            byte[] screenshotAsJPG = screenshot.EncodeToJPG();
-            if(!Directory.Exists(path)) {
+                sendMessage("*SNAP*");
 
-                Directory.CreateDirectory(path);
+                // wait till the last possible moment before screen rendering to hide UI
+                bool canvasWasEnabled = UICanvas.enabled;
+                yield return null;
+                UICanvas.enabled = false;
 
-            }
-            File.WriteAllBytes(path + timestmp + ".jpg", screenshotAsJPG);
+                // wait for screen rendering to complete
+                yield return new WaitForEndOfFrame();
+                if(canvasWasEnabled) UICanvas.enabled = true;
 
-            List<BoxData> boxDataList = new List<BoxData>();
+                // take screenshot and save raw version
+                Texture2D screenshot = ScreenCapture.CaptureScreenshotAsTexture();
+                byte[] screenshotAsJPG = screenshot.EncodeToJPG();
+                if(!Directory.Exists(path)) {
 
-            foreach(GameObject detectable in detectables) {
+                    Directory.CreateDirectory(path);
 
-                Renderer renderer = detectable.GetComponent<Renderer>();
+                }
+                File.WriteAllBytes(path + timestmp + randomNumber + ".jpg", screenshotAsJPG);
 
-                // get boundary box
-                if(renderer.isVisible) {
+                List<BoxData> boxDataList = new List<BoxData>();
 
-                    // get an array of all vertices of the object
-                    Mesh mesh = detectable.GetComponent<MeshFilter>().mesh;
-                    Vector3[] vertices = mesh.vertices;
+                foreach(GameObject detectable in detectables) {
 
-                    // since vertices are given relative to their gameobject, we need to convert them into worldSpace
-                    for(int i = 0; i < vertices.Length; i++) {
+                    Renderer renderer = detectable.GetComponent<Renderer>();
 
-                        vertices[i] = detectable.transform.TransformPoint(vertices[i]);
+                    // get boundary box
+                    if(renderer.isVisible) {
 
-                    }
+                        // get an array of all vertices of the object
+                        Mesh mesh = detectable.GetComponent<MeshFilter>().mesh;
+                        Vector3[] vertices = mesh.vertices;
 
-                    float left = Screen.width + 1f;
-                    float right = -1f;
-                    float top = -1f;
-                    float bottom = Screen.height + 1f;
+                        // since vertices are given relative to their gameobject, we need to convert them into worldSpace
+                        for(int i = 0; i < vertices.Length; i++) {
 
-                    // with camera.WorldToScreenPoint(Vector3 worldPoint) search for the top, bottom, most left and most right point
-                    bool objectInSight = false;
-                    foreach(Vector3 vertex in vertices) {
+                            vertices[i] = detectable.transform.TransformPoint(vertices[i]);
 
-                        // check if vertex is NOT BEHIND the camera
-                        if(Vector3.Dot(playerCamera.transform.forward, vertex - playerCamera.transform.position) >= 0f) {
+                        }
 
-                            // check if corresponding screen point is on screen and if it is a newly found border point candidate
-                            Vector3 screenPoint = playerCamera.WorldToScreenPoint(vertex);
-                            if(new Rect(0, 0, Screen.width, Screen.height).Contains(screenPoint)) {
+                        float left = Screen.width + 1f;
+                        float right = -1f;
+                        float top = -1f;
+                        float bottom = Screen.height + 1f;
 
-                                // check if vertex is obscured by another non-detectable object
-                                bool vertexIsObscured = false;
-                                RaycastHit hitInfo;
-                                if(Physics.Raycast( playerCamera.transform.position,
-                                                    vertex - playerCamera.transform.position,
-                                                    out hitInfo,
-                                                    (vertex - playerCamera.transform.position).magnitude - 0.01f)) {
+                        // with camera.WorldToScreenPoint(Vector3 worldPoint) search for the top, bottom, most left and most right point
+                        bool objectInSight = false;
+                        foreach(Vector3 vertex in vertices) {
 
-                                    if(hitInfo.collider.gameObject != detectable) {
+                            // check if vertex is NOT BEHIND the camera
+                            if(Vector3.Dot(playerCamera.transform.forward, vertex - playerCamera.transform.position) >= 0f) {
 
-                                        vertexIsObscured = true;
+                                // check if corresponding screen point is on screen and if it is a newly found border point candidate
+                                Vector3 screenPoint = playerCamera.WorldToScreenPoint(vertex);
+                                if(new Rect(0, 0, Screen.width, Screen.height).Contains(screenPoint)) {
 
-                                    } else {
+                                    // check if vertex is obscured by another object
+                                    bool vertexIsObscured = false;
+                                    RaycastHit hitInfo;
+                                    if(Physics.Raycast(playerCamera.transform.position,
+                                                        vertex - playerCamera.transform.position,
+                                                        out hitInfo,
+                                                        (vertex - playerCamera.transform.position).magnitude - 0.01f)) {
 
-                                        objectInSight = true;
+                                        if(hitInfo.collider.gameObject != detectable) {
+
+                                            vertexIsObscured = true;
+
+                                        }
+                                        else {
+
+                                            objectInSight = true;
+
+                                        }
+                                    }
+
+                                    if(!vertexIsObscured) {
+
+                                        if(screenPoint.x < left) left = screenPoint.x;
+                                        if(screenPoint.x > right) right = screenPoint.x;
+                                        if(screenPoint.y < bottom) bottom = screenPoint.y;
+                                        if(screenPoint.y > top) top = screenPoint.y;
 
                                     }
                                 }
-
-                                if(!vertexIsObscured) {
-
-                                    if(screenPoint.x < left) left = screenPoint.x;
-                                    if(screenPoint.x > right) right = screenPoint.x;
-                                    if(screenPoint.y < bottom) bottom = screenPoint.y;
-                                    if(screenPoint.y > top) top = screenPoint.y;
-
-                                }
                             }
                         }
+
+                        if(objectInSight) {
+
+                            // represent detected objects in the info box
+                            string name = detectable.name;
+                            if(detectable.name.Contains("Cube")) name = "<color=red>" + detectable.name + "</color>";
+                            else if(detectable.name.Contains("Ball")) name = "<color=cyan>" + detectable.name + "</color>";
+                            else Debug.Log("Couldn't recognize object name when colorizing its name.");
+                            sendMessage(name + "(id: " + detectable.GetInstanceID() + ") snapped");
+
+                            // draw the lines
+                            for(int x = (int)left; x <= (int)right; x++) {
+                                screenshot.SetPixel(x, (int)bottom, Color.red);
+                                screenshot.SetPixel(x, (int)top, Color.red);
+                            }
+                            for(int y = (int)bottom; y <= (int)top; y++) {
+                                screenshot.SetPixel((int)left, y, Color.red);
+                                screenshot.SetPixel((int)right, y, Color.red);
+                            }
+
+                            // gather corresponding data
+                            int id = -1;
+                            if(detectable.name.StartsWith("Cube")) id = 0;
+                            else if(detectable.name.StartsWith("Ball")) id = 1;
+                            else Debug.Log("Couldn't infer object class");
+
+                            int width = (int)right - (int)left;
+                            int height = (int)top - (int)bottom;
+                            int center_x = (int)left + width / 2;
+                            int center_y = (int)bottom + height / 2;
+
+                            BoxData boxData = new BoxData(id,
+                                                            center_x,
+                                                            center_y,
+                                                            width,
+                                                            height);
+                            boxDataList.Add(boxData);
+
+                        }
                     }
+                }
 
-                    if(objectInSight) {
+                // save labeled image
+                screenshotAsJPG = screenshot.EncodeToJPG();
+                File.WriteAllBytes(path + timestmp + randomNumber + "_labeled.jpg", screenshotAsJPG);
 
-                        // represent detected objects in the info box
-                        string name = detectable.name;
-                        if(detectable.name.Contains("Cube")) name = "<color=red>" + detectable.name + "</color>";
-                        else if(detectable.name.Contains("Ball")) name = "<color=cyan>" + detectable.name + "</color>";
-                        else Debug.Log("Couldn't recognize object name when colorizing its name.");
-                        sendMessage(name + "(id: " + detectable.GetInstanceID() + ") snapped");
+                /* // save json data
+                string jsonString = JsonUtility.ToJson(new BoxDataList(boxDataList), true);
+                File.WriteAllText(path + timestmp + ".json", jsonString);*/
 
-                        // draw the lines
-                        for(int x = (int)left; x <= (int)right; x++) {
-                            screenshot.SetPixel(x, (int)bottom, Color.red);
-                            screenshot.SetPixel(x, (int)top, Color.red);
-                        }
-                        for(int y = (int)bottom; y <= (int)top; y++) {
-                            screenshot.SetPixel((int)left, y, Color.red);
-                            screenshot.SetPixel((int)right, y, Color.red);
-                        }
+                // save txt file
+                foreach(BoxData boxData in boxDataList) {
 
-                        // gather corresponding data
-                        int id = -1;
-                        if(detectable.name.StartsWith("Cube")) id = 0;
-                        else if(detectable.name.StartsWith("Ball")) id = 1;
-                        else Debug.Log("Couldn't infer object class");
+                    using(StreamWriter sw = File.AppendText(path + timestmp + randomNumber + ".txt")) {
 
-                        int width = (int)right - (int)left;
-                        int height = (int)top - (int)bottom;
-                        int center_x = (int)left + width / 2;
-                        int center_y = (int)bottom + height / 2;
-
-                        BoxData boxData = new BoxData(id,
-                                                        center_x,
-                                                        center_y,
-                                                        width,
-                                                        height);
-                        boxDataList.Add(boxData);
+                        sw.WriteLine(boxData.id + " " +
+                                        boxData.pos_x + " " +
+                                        boxData.pos_y + " " +
+                                        boxData.width + " " +
+                                        boxData.height);
 
                     }
                 }
-            }
 
-            // save labeled image
-            screenshotAsJPG = screenshot.EncodeToJPG();
-            File.WriteAllBytes(path + timestmp + "_labeled.jpg", screenshotAsJPG);
+                if(boxDataList.Count == 0) using(StreamWriter sw = File.AppendText(path + timestmp + randomNumber + ".txt")) {
 
-            /* // save json data
-            string jsonString = JsonUtility.ToJson(new BoxDataList(boxDataList), true);
-            File.WriteAllText(path + timestmp + ".json", jsonString);*/
+                        sw.WriteLine("");
 
-            // save txt file
-            foreach(BoxData boxData in boxDataList) {
-
-                using (StreamWriter sw = File.AppendText(path + timestmp + ".txt")) {
-                    sw.WriteLine(   boxData.id + " " +
-                                    boxData.pos_x + " " +
-                                    boxData.pos_y + " " +
-                                    boxData.width + " " +
-                                    boxData.height);
                 }
             }
+            else sendMessage("<color=red>Nothing captured. Please wait at least one second. That's because the " +
+                              "screenshots are named with a timestamp messured in seconds. Multiple screenshots " + 
+                              "within the same second would lead to overwritten images and faulty labels.</color>");
         }
     }
 
