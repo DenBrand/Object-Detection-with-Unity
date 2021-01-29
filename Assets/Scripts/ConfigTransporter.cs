8afb0da658cd467dafcfb564ba8c5fa8aa4d0175
DenBrand <dennis.brand-r5c@rub.de>
2021-01-29 15:32:55 +0100

    Created configure scene and container class ConfigTransport to tranfer the chosen options into the main scene.

---
+++ Assets/Scripts/ConfigTransporter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConfigTransporter : MonoBehaviour
+{
+    public static ConfigTransporter instance;
+    private bool randomizeColors;
+    private bool varyLighting;
+    private bool addNoise;
+    private bool smoothImages;
+
+    private void Start() {
+
+        randomizeColors = false;
+        varyLighting = false;
+        addNoise = false;
+        smoothImages = false;
+
+    }
+
+    private void Awake() {
+        if(instance == null) instance = this;
+        else if(instance != this) Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadMainScene() {
+
+        SceneManager.LoadScene(1);
+
+    }
+}
